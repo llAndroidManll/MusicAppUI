e9d5e22399ce122ebf0911a4fa5236b53d6abe8b
llAndroidManll <153990813+llAndroidManll@users.noreply.github.com>
2024-03-11 18:02:23 +0400

    Creating  sealed class and  sealed class

--- app/src/main/java/sahak/sahakyan/musicappui/Screen.kt
+++ app/src/main/java/sahak/sahakyan/musicappui/Screen.kt
@@ -1,4 +1,25 @@
 package sahak.sahakyan.musicappui
 
-class Screen {
+import android.icu.text.CaseMap.Title
+import androidx.annotation.DrawableRes
+
+sealed class Screen(
+    val title: String,
+    val route: String,
+) {
+    sealed class DrawerScreen(
+        val dTitle: String,
+        val dRoute: String,
+        @DrawableRes val icon: Int
+    ): Screen(dTitle,dRoute) {
+        data object Account: DrawerScreen(
+            "Account", "account", R.drawable.account_circle
+        )
+        data object Subscription: DrawerScreen(
+            "Subscription", "subscribe", R.drawable.subscribe
+        )
+        data object AddAccount: DrawerScreen(
+            "Add Account", "add_account", R.drawable.baseline_person_add_alt_1_24
+        )
+    }
 }
