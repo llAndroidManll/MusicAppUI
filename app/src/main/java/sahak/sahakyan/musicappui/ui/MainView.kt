1449bd1a84626b5eecbb4d0a1dc608401465c471
llAndroidManll <153990813+llAndroidManll@users.noreply.github.com>
2024-03-11 18:37:59 +0400

    Making the drawer appear -- Adding drawerContent parameter in Scaffold and Adding Current Route Logic

--- app/src/main/java/sahak/sahakyan/musicappui/ui/MainView.kt
+++ app/src/main/java/sahak/sahakyan/musicappui/ui/MainView.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
@@ -13,18 +15,25 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import sahak.sahakyan.musicappui.Screen
+import sahak.sahakyan.musicappui.screenInDrawer
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -45,6 +54,16 @@ fun MainView(
     * So that's why we need to create a CoroutineScope, which will allow us to launch suspend function (opening drawer)
     * */
     val scope: CoroutineScope = rememberCoroutineScope()
+
+    // Allow us to find out on which "View" we are currently
+    val controller: NavController = rememberNavController()
+    val navBackStackEntry by controller.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    val title = remember {
+        // TODO Change that to currentScreen.title
+        mutableStateOf("")
+    }
 
     Scaffold(
         modifier = Modifier,
@@ -73,10 +92,26 @@ fun MainView(
                  }
              )
         },
-        bottomBar = {},
-        snackbarHost = {},
-        floatingActionButton = {},
-
+        scaffoldState = scaffoldState,
+        drawerContent = {
+            LazyColumn(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                items(screenInDrawer) { item ->
+                    DrawerItem(selected = currentRoute == item.dRoute, item = item) {
+                        scope.launch {
+                            scaffoldState.drawerState.close()
+                        }
+                        if(item.dRoute == "add_account") {
+                            // open dialog
+                        } else {
+                            controller.navigate(item.dRoute)
+                            title.value = item.dTitle
+                        }
+                    }
+                }
+            }
+        }
     ) {
         Text(text = "Text", modifier = Modifier.padding(it))
     }
@@ -92,8 +127,10 @@ fun DrawerItem(
     val background = if (selected) Color.DarkGray else Color.White
 
     Row(
-        Modifier.fillMaxWidth()
-            .padding(horizontal = 8.dp, vertical = 16.dp).background(background)
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+            .background(background)
             .clickable {
                 onDrawerItemClicked()
             }
