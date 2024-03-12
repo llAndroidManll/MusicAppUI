4e84b0eea84511879bd6d01f682be42deb650820
llAndroidManll <153990813+llAndroidManll@users.noreply.github.com>
2024-03-12 17:41:45 +0400

    Creating MainViewModel where we are saving our current screen value

--- app/src/main/java/sahak/sahakyan/musicappui/ui/MainView.kt
+++ app/src/main/java/sahak/sahakyan/musicappui/ui/MainView.kt
@@ -2,6 +2,7 @@ package sahak.sahakyan.musicappui.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -27,11 +28,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import sahak.sahakyan.musicappui.MainViewModel
 import sahak.sahakyan.musicappui.Screen
 import sahak.sahakyan.musicappui.screenInDrawer
 
@@ -42,7 +48,7 @@ fun MainView(
 ) {
 
     /*
-    *   And why do we need something called a scaffold state,
+    *   We need something called a scaffold state,
     *   which takes care of the state of the scaffold entirely(ամբողջությամբ).
     *   Basically the scaffold is the parent of UI element or the view for the entire page,
     *   which contains the top bar, which contains the content, which contains the bottom bar and etc.
@@ -55,14 +61,24 @@ fun MainView(
     * */
     val scope: CoroutineScope = rememberCoroutineScope()
 
+    val viewModel: MainViewModel = viewModel()
+
     // Allow us to find out on which "View" we are currently
     val controller: NavController = rememberNavController()
     val navBackStackEntry by controller.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
 
+
+    val currentScreen = remember {
+        viewModel.currentScreen.value
+    }
+
+    val dialogOpen = remember {
+        mutableStateOf(false)
+    }
+
     val title = remember {
-        // TODO Change that to currentScreen.title
-        mutableStateOf("")
+        mutableStateOf(currentScreen.title)
     }
 
     Scaffold(
@@ -73,7 +89,7 @@ fun MainView(
                  navigationIcon = {
                      IconButton(onClick = {
                         /* TODO OPEN THE DRAWER*/
-                         /* Opening a drawer is synchronize method(գործողություն), it's a suspend function that happens. ---(Look at 27 line)---
+                         /* Opening a drawer is synchronize method(գործողություն), it's a suspend function that happens. ---(Look at  *We need something called a scaffold state,...)---
                          * */
                          /*
                          *  So this one will be our open drawer launch where I'm just going to use the scope to launch the following code.
@@ -103,7 +119,7 @@ fun MainView(
                             scaffoldState.drawerState.close()
                         }
                         if(item.dRoute == "add_account") {
-                            // open dialog
+                            dialogOpen.value = true
                         } else {
                             controller.navigate(item.dRoute)
                             title.value = item.dTitle
@@ -113,7 +129,8 @@ fun MainView(
             }
         }
     ) {
-        Text(text = "Text", modifier = Modifier.padding(it))
+        Navigation(navController = controller, viewModel = viewModel, pd = it)
+        AccountDialog(dialogOpen = dialogOpen)
     }
 }
 
@@ -144,5 +161,25 @@ fun DrawerItem(
             text = item.dTitle,
             style = MaterialTheme.typography.h5,
         )
+    }
+}
+
+@Composable
+fun Navigation(
+    navController: NavController,
+    viewModel: MainViewModel,
+    pd: PaddingValues
+) {
+    NavHost(
+        navController = navController as NavHostController,
+        startDestination = Screen.DrawerScreen.AddAccount.route,
+        modifier = Modifier.padding(pd),
+    ) {
+        composable(Screen.DrawerScreen.AddAccount.route) {
+
+        }
+        composable(Screen.DrawerScreen.Subscription.route) {
+//            SubscriptionScreen(viewModel)
+        }
     }
 }
