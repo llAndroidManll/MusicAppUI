2a15099367a005926a33fa835b024f11d5450516
llAndroidManll <153990813+llAndroidManll@users.noreply.github.com>
2024-03-14 19:15:43 +0400

    Creating BrowseScreen

--- app/src/main/java/sahak/sahakyan/musicappui/ui/BrowseScreen.kt
+++ app/src/main/java/sahak/sahakyan/musicappui/ui/BrowseScreen.kt
@@ -1,2 +1,20 @@
 package sahak.sahakyan.musicappui.ui
 
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.lazy.grid.GridCells
+import sahak.sahakyan.musicappui.R
+
+@Composable
+fun BrowseScreen() {
+    val categories = listOf<String>("Hits", "Happy", "Workout", "Running", "TGIF", "Yoga")
+
+    LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+        items(categories) {
+            cat ->
+            BrowseItem(cat = cat, drawable = R.drawable.browse)
+        }
+    }
+}
